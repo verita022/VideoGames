2f7d745b4007f3bea672c6f515c2891024fbdb9c
Veronica Camisassa <verita022@gmail.com>
2021-11-12 23:26:49 -0300

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -19,8 +19,8 @@ server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://gamesportal.herokuapp.com/videogame'); // deberia ir url de produccion
+server.all('/*', function(req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // deberia ir url de produccion
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
