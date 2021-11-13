3e9c97933bd2acd6469bc6d4ebfae2a057183d41
verita022 <verita022@gmail.com>
2021-11-12 21:06:12 -0300

    pasos restantes en localhost para deploy

--- api/src/app.js
+++ api/src/app.js
@@ -10,14 +10,17 @@ require('./db.js');
 
 const server = express();
 
+const cors = require('cors')
+
 server.name = 'API';
 
+server.use(cors())
 server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*'); // deberia ir url de produccion
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
