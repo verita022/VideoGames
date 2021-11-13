7e74c4fdc5c45c7f3620965f3379da49664b39af
Veronica Camisassa <verita022@gmail.com>
2021-11-12 23:53:59 -0300

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -9,17 +9,22 @@ require('dotenv').config();
 require('./db.js');
 
 const server = express();
-
 const cors = require('cors')
 
 server.name = 'API';
+
 
 server.use(cors())
 server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.options('*', cors());
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+  });
 
 server.use(express.json())
 server.use('/', routes);
