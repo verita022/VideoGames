ac057704c3faaac521564b9d80d0952023d38d26
Veronica Camisassa <verita022@gmail.com>
2021-11-12 22:55:19 -0300

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -23,7 +23,6 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // deberia ir url de produccion
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
 
